2247e784e1d0da1c40f23cdcaa310b285aef76cd
RockingWithTheBest <151398987+RockingWithTheBest@users.noreply.github.com>
2024-08-19 13:15:47 +0300

    files added of sliders first folders

---
+++ java_slider.js
@@ -0,0 +1,23 @@
+function imgSlider(anything){
+    document.querySelector(".food-list").src = anything;
+}
+
+let icons = document.querySelectorAll(".lists li");
+
+//Function to add active
+function toggle(e){
+    const oldIcons = document.querySelectorAll(".lists li");
+    oldIcons.forEach(icon => icon.classList.remove("active"));
+
+    //add activeon Click
+    this.classList.add("active");
+
+    //change slider image
+    const img = this.children[0].src;
+    imgSlider(img);
+}
+
+//Add event llistener
+icons.forEach((icon) =>{
+    icon.addEventListener("click", toggle)
+});
